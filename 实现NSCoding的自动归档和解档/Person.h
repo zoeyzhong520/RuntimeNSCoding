0038e92d0165570cc80020bdeb34c2643a44e97b
AsiaZhang <17612156438@sina.cn>
2019-09-25 11:11:07 +0800

    实现NSCoding的自动归档和解档

--- 实现NSCoding的自动归档和解档/Person.h
+++ 实现NSCoding的自动归档和解档/Person.h
@@ -10,7 +10,9 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface Person : NSObject
+@interface Person : NSObject<NSCoding>
+
+@property (nonatomic, copy) NSString *name;
 
 @end
 
--- 实现NSCoding的自动归档和解档/Person.m
+++ 实现NSCoding的自动归档和解档/Person.m
@@ -7,7 +7,36 @@
 //
 
 #import "Person.h"
+#import <objc/message.h>
 
 @implementation Person
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super init];
+    if (self) {
+        unsigned int count;
+        Ivar *ivars = class_copyIvarList([self class], &count);
+        for (int i = 0; i < count; i ++) {
+            Ivar ivar = ivars[i];
+            NSString *key = [NSString stringWithUTF8String:ivar_getName(ivar)];
+            //模型属性赋值
+            [self setValue:[aDecoder decodeObjectForKey:key] forKey:key];
+        }
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+    unsigned int count;
+    Ivar *ivars = class_copyIvarList([self class], &count);
+    for (int i = 0; i < count; i ++) {
+        Ivar ivar = ivars[i];
+        NSString *key = [NSString stringWithUTF8String:ivar_getName(ivar)];
+        //模型属性归档
+        [aCoder encodeObject:[self valueForKey:key] forKey:key];
+    }
+}
 
 @end
